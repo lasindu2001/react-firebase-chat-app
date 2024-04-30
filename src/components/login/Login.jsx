22665b489fb3f965ccb834ffb8e94701f09b04b5
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-01 01:09:02 +0530

    add log in functionality

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import "./login.css"
 import { toast } from "react-toastify"
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import { auth, db } from "../../lib/firebase"
 import { doc, setDoc } from "firebase/firestore"
 import upload from "../../lib/upload"
@@ -46,8 +46,19 @@ const Login = () => {
             setLoading(false)
         }
     }
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault()
+        setLoading(true)
+        const formData = new FormData(e.target)
+        const { email, password } = Object.fromEntries(formData)
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
     }
     return (
         <div className="login">
